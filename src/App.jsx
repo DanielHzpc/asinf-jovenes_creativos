313ccfb9fceb36de8694586bf44d75342cfeb653
Daniel <danielhzpc@gmail.com>
2024-11-15 22:28:40 -0500

    Incorporo los archivos que tenia antes de crear el GitHub original

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import React from 'react'
 import './App.css'
+import MainPage from './pages/mainPage'
+import NavBar from './components/MainPage/NavBar'
+import Footer from './components/MainPage/Footer'
 
 function App() {
 
   return (
     <>
-      <p>INICIO</p>
+      <NavBar />
+      <MainPage />
+      <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+import {NextUIProvider} from "@nextui-org/react";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  }
+]) 
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
+    
   </StrictMode>,
 )
